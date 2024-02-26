9dfbd430ae8a8d25ceb356ef6955402394216183
Richard Holmes <richard@shedc.uk>
2024-02-26 17:03:09 +0000

    Add Initial Tests and Auto Approve debendabot

---
+++ tests/fixtures/test_init.py
@@ -0,0 +1,7 @@
+"""Test leakbot setup process."""
+
+from homeassistant.core import HomeAssistant
+
+async def test_setup_unload_and_reload_entry(hass: HomeAssistant):
+    """Test a configurated instance loads and unloads."""
+    pass
