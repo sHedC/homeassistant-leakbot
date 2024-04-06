d318222f98a5b2c153b8f0a3e1c893529d4495ef
Richard Holmes <richard@shedc.uk>
2024-04-06 09:19:15 +0000

    Add first State.

--- custom_components/leakbot/sensor.py
+++ custom_components/leakbot/sensor.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 
 from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -13,8 +14,8 @@ from .entity import LeakbotEntity
 
 ENTITY_DESCRIPTIONS = (
     SensorEntityDescription(
-        key="leakbot",
-        name="Integration Sensor",
+        key="device_status",
+        name="Device Status",
         icon="mdi:format-quote-close",
     ),
 )
@@ -24,14 +25,16 @@ async def async_setup_entry(
     hass: HomeAssistant, entry: ConfigEntry, async_add_devices: AddEntitiesCallback
 ):
     """Set up the sensor platform."""
-    coordinator = hass.data[DOMAIN][entry.entry_id]
-    async_add_devices(
-        LeakbotSensor(
-            coordinator=coordinator,
-            entity_description=entity_description,
-        )
-        for entity_description in ENTITY_DESCRIPTIONS
-    )
+    coordinator: LeakbotDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
+
+    entities: list[LeakbotEntity] = []
+    devices: dict[str, any] = coordinator.data.get("devices", {})
+    for _, device in devices.items():
+        for entity_description in ENTITY_DESCRIPTIONS:
+            entities.append(LeakbotSensor(coordinator, device, entity_description))
+
+    async_add_devices(entities, True)
+    coordinator.remove_old_entities(Platform.SENSOR)
 
 
 class LeakbotSensor(LeakbotEntity, SensorEntity):
@@ -40,13 +43,18 @@ class LeakbotSensor(LeakbotEntity, SensorEntity):
     def __init__(
         self,
         coordinator: LeakbotDataUpdateCoordinator,
+        device: dict[str, any],
         entity_description: SensorEntityDescription,
     ) -> None:
         """Initialize the sensor class."""
-        super().__init__(coordinator)
+        self._device_id = device["id"]
+        super().__init__(
+            coordinator, f"{self._device_id}_{entity_description.key}", Platform.SENSOR
+        )
         self.entity_description = entity_description
 
     @property
     def native_value(self) -> str:
         """Return the native value of the sensor."""
-        return "test"
+        # Here we get from data->devices->device_id->device_status
+        return self.coordinator.data["devices"][self._device_id]["device_status"]
---
+++ tests/test_sensor.py
@@ -0,0 +1,58 @@
+"""Leakbot Sensor Tests."""
+
+from unittest.mock import patch
+import pytest
+
+from aiohttp.web import Application
+
+from homeassistant.core import HomeAssistant
+from homeassistant.const import Platform
+
+from pytest_homeassistant_custom_component.common import MockConfigEntry
+
+from custom_components.leakbot.api import LeakbotApiClient
+from custom_components.leakbot.const import DOMAIN
+from custom_components.leakbot.coordinator import LeakbotDataUpdateCoordinator
+
+from .conftest import ClientSessionGenerator, VALID_LOGIN
+
+
+@pytest.fixture(autouse=True)
+def override_entity():
+    """Override the ENTITIES to test Sensors."""
+    with patch(
+        "custom_components.leakbot.PLATFORMS",
+        [Platform.SENSOR],
+    ):
+        yield
+
+
+async def test_sensor_setup(
+    hass: HomeAssistant,
+    leakbot_api: Application,
+    aiohttp_client: ClientSessionGenerator,
+):
+    """Test Sensors are Created and Updated."""
+    session = await aiohttp_client(leakbot_api)
+    entry = MockConfigEntry(domain=DOMAIN, data=VALID_LOGIN)
+    entry.add_to_hass(hass)
+
+    # hass.data.setdefault(DOMAIN, {})
+    # coordinator = LeakbotDataUpdateCoordinator(hass, api, entry.entry_id)
+    # hass.data[DOMAIN][entry.entry_id] = coordinator
+
+    with patch(
+        "custom_components.leakbot.async_get_clientsession",
+        return_value=session,
+    ):
+        await hass.config_entries.async_setup(entry.entry_id)
+        await hass.async_block_till_done()
+
+    # Check we called the Mock and we have a Sensor.
+    assert (
+        hass.states.async_entity_ids_count(Platform.SENSOR) > 0
+    ), "Sensors Failed to Create"
+
+    state = hass.states.get("sensor.leakbot_123456_device_status")
+    assert state.state == "Leak Inactive"
+    assert state.name == "Device Status"
