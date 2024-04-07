b6418c51364cefef13734d133cd755cfc4979e20
Richard Holmes <richard@shedc.uk>
2024-04-07 15:50:54 +0000

    Add Device Info

--- custom_components/leakbot/entity.py
+++ custom_components/leakbot/entity.py
@@ -6,7 +6,7 @@ from homeassistant.helpers.entity import DeviceInfo
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 from homeassistant.util import slugify
 
-from .const import ATTRIBUTION, DOMAIN, NAME, VERSION
+from .const import ATTRIBUTION, DOMAIN, NAME
 from .coordinator import LeakbotDataUpdateCoordinator
 
 
@@ -32,15 +32,20 @@ class LeakbotEntity(CoordinatorEntity):
 
         self.entity_id = f"{platform}.{self._attr_unique_id}"
 
-        self._attr_device_info = DeviceInfo(
-            identifiers={(DOMAIN, id)},
-            name=NAME,
-            model=VERSION,
-            manufacturer=NAME,
-        )
-
     @property
     def get_device_data(self) -> dict[str, any]:
         """Get the device data."""
         data = self.coordinator.data["devices"][self._device_id]
         return data
+
+    @property
+    def device_info(self):
+        """Return the device information."""
+        return DeviceInfo(
+            identifiers={(DOMAIN, self._device_id)},
+            name=NAME,
+            manufacturer=NAME,
+            model=self.get_device_data["device_type"],
+            sw_version=self.get_device_data["fw_version"],
+            via_device=(DOMAIN, self.get_device_data["leakbotId"]),
+        )
