b16f6fe603cab4362489b9047301d9e9b56cc929
Richard Holmes <richard@shedc.uk>
2024-04-08 11:55:38 +0000

    Add Battery and Leak Free Days

--- custom_components/leakbot/sensor.py
+++ custom_components/leakbot/sensor.py
@@ -2,9 +2,15 @@
 
 from __future__ import annotations
 
-from homeassistant.components.sensor import SensorEntity, SensorEntityDescription
+from dataclasses import dataclass
+
+from homeassistant.components.sensor import (
+    SensorEntity,
+    SensorEntityDescription,
+    SensorDeviceClass,
+)
 from homeassistant.config_entries import ConfigEntry
-from homeassistant.const import Platform
+from homeassistant.const import Platform, UnitOfTime
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
@@ -12,11 +18,33 @@ from .const import DOMAIN
 from .coordinator import LeakbotDataUpdateCoordinator
 from .entity import LeakbotEntity
 
+
+@dataclass
+class LeakbotSensorEntityDescription(SensorEntityDescription):
+    """Leakbot Sensor Entity Description."""
+
+    lookup_keys: str = None
+
+
 ENTITY_DESCRIPTIONS = (
-    SensorEntityDescription(
+    LeakbotSensorEntityDescription(
         key="device_status",
         name="Device Status",
         icon="mdi:water-check-outline",
+    ),
+    LeakbotSensorEntityDescription(
+        lookup_keys="info",
+        key="battery_sm",
+        name="Battery Status",
+        icon="mdi:battery80",
+    ),
+    LeakbotSensorEntityDescription(
+        lookup_keys="info.leak_count_summary",
+        key="leak_free_days",
+        name="Leak Free Days",
+        device_class=SensorDeviceClass.DURATION,
+        native_unit_of_measurement=UnitOfTime.DAYS,
+        suggested_unit_of_measurement=UnitOfTime.DAYS,
     ),
 )
 
@@ -26,7 +54,6 @@ async def async_setup_entry(
 ):
     """Set up the sensor platform."""
     coordinator: LeakbotDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id]
-
     entities: list[LeakbotEntity] = []
     devices: dict[str, any] = coordinator.data.get("devices", {})
     for _, device in devices.items():
@@ -44,16 +71,22 @@ class LeakbotSensor(LeakbotEntity, SensorEntity):
         self,
         coordinator: LeakbotDataUpdateCoordinator,
         device: dict[str, any],
-        entity_description: SensorEntityDescription,
+        entity_description: LeakbotSensorEntityDescription,
     ) -> None:
         """Initialize the sensor class."""
         super().__init__(
             Platform.SENSOR, coordinator, device["id"], entity_description.key
         )
-        self.entity_description = entity_description
+        self.entity_description: LeakbotSensorEntityDescription = entity_description
 
     @property
     def native_value(self) -> str:
         """Return the native value of the sensor."""
-        # Here we get from data->devices->device_id->device_status
-        return self.get_device_data["device_status"]
+        sub_data = self.get_device_data
+
+        # Get the Data Subset.
+        if self.entity_description.lookup_keys:
+            for sub_key in self.entity_description.lookup_keys.split("."):
+                sub_data = sub_data[sub_key]
+
+        return sub_data[self._key]
